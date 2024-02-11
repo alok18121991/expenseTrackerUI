4b9fe174e6bb193850baa9e3c98922b3ac56580c
Alok Kumar Singh <alok18121991@gmail.com>
2024-02-11 22:36:13 +0530

    expense history added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import './App.css';
 import AddExpense from './components/Expense/addExpense'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Col, Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <AddExpense></AddExpense>
-      </header>
+      <div className="App-header">
+        <Container className='app-container'>
+          <Row>
+            <Col xs lg="1" className='nav-bar'>
+              <nav class="nav flex-column">
+                <a class="nav-link" href="#">Active</a>
+                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#">Link</a>
+                <a class="nav-link" href="#">Disabled</a>
+              </nav>
+            </Col>
+            <Col>
+              <AddExpense></AddExpense>
+            </Col>
+          </Row>
+
+        </Container>
+      </div>
     </div>
   );
 }
